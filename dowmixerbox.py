435d0eeacbe53899aab58fbd0f03a8e116669d24
huihui <levis9012980@gmail.com>
2016-01-26 17:06:41 +0800

    整理： 1.程式整理

--- dowmixerbox.py
+++ dowmixerbox.py
@@ -3,13 +3,11 @@ import urllib.request
 import youtube_dl
 import json
 import re
-# from bs4 import BeautifulSoup
 
 class mixerbox:
 	def getvectorId(self,url):
 		try:
 			return re.search(r'http\:\/\/www\.mixerbox\.com\/list\/([0-9]+)',url).group(1)
-			# return vectorId
 			pass
 		except:
 			print("網址輸入錯誤")
@@ -28,30 +26,23 @@ class mixerbox:
 		}
 		for x in item:
 			url = "http://www.youtube.com/watch?v="+x["f"]
-			with youtube_dl.YoutubeDL(options) as ydl:
-				ydl.download([url])
-			# try:
-			# 	url = "http://www.youtube.com/watch?v="+x["f"]
-			# 	with youtube_dl.YoutubeDL(options) as ydl:
-			# 		ydl.download([url])
-			# 	pass
-			# except:
-			# 	# print("========檔名有問題=========")
-			# 	# print("代碼:"+x["f"])
-			# 	# print(x["tt"].encode('utf-8'))
-			# 	# print("========檔名有問題=========")
-			# 	pass
+			try:
+				with youtube_dl.YoutubeDL(options) as ydl:
+					ydl.download([url])
+				pass
+			except:
+				pass
+			
+
+	def getHTML(self,vectorId):
+		cto = urllib.request.urlopen('http://www.mixerbox.com/service?&callback=jQuery172009174624213601712_1453105380546&appVer=205&funcs=getVector&skip=0&limit=0&locale=zh-tw&mobile=1&type=playlist&vectorId='+vectorId+'&random=8398&_=1453105381259')
+		return cto.read().decode('utf-8')
 
 mixerbox = mixerbox()
-InputUrl = input("請輸入收藏清單網址：")
-vectorId = mixerbox.getvectorId(InputUrl)
-# vectorId = re.search(r'http\:\/\/www\.mixerbox\.com\/list\/([0-9]+)',InputUrl).group(1)
-# vectorId = "7742969"
-cto = urllib.request.urlopen('http://www.mixerbox.com/service?&callback=jQuery172009174624213601712_1453105380546&appVer=205&funcs=getVector&skip=0&limit=0&locale=zh-tw&mobile=1&type=playlist&vectorId='+vectorId+'&random=8398&_=1453105381259')
-HTML = cto.read().decode('utf-8')
-# HTML = cto.read()
-# soup = BeautifulSoup(HTML,"html5lib")
-# print(soup)
+# InputUrl = input("請輸入收藏清單網址：")
+# vectorId = mixerbox.getvectorId(InputUrl)
+vectorId = "7742969"
+HTML = mixerbox.getHTML(vectorId)
 result = re.search(r'\((.+?)\)\;',HTML).group(1)
 res = json.loads(result)
 item = res["getVector"]["items"]
