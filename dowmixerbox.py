215fa87a1e418c27a4016e7dbb9844ce24c75520
胖huihui <levis9012980@gmail.com>
2019-04-19 16:25:25 +0800

    修改下載音樂模式

--- dowmixerbox.py
+++ dowmixerbox.py
@@ -3,20 +3,21 @@ import urllib.request
 import youtube_dl
 import json
 import re
+import listtt
 
 class mixerbox:
-	def getvectorId(self,url):
+	def getvectorId(self, url):
 		try:
-			return re.search(r'http\:\/\/www\.mixerbox\.com\/list\/([0-9]+)',url).group(1)
+			return re.search(r'http\:\/\/www\.mixerbox\.com\/list\/([0-9]+)', url).group(1)
 			pass
 		except:
 			print("網址輸入錯誤")
 			exit()
 
-	def dowload(self,item):
+	def dowload(self, item):
 		options = {
 			'verbose': True,
-			'format': 'mp4',
+			'format': 'bestaudio/best',
 			'outtmpl': 'C:\\Users\\user\\Downloads\\music\\%(title)s.%(ext)s',
 			'noplaylist' : True,
 			'postprocessors': [{
@@ -25,8 +26,8 @@ class mixerbox:
 			}],
 		}
 		for x in item:
-			url = "http://www.youtube.com/watch?v="+x["f"]
 			try:
+				url = "http://www.youtube.com/watch?v=" + x["f"]
 				with youtube_dl.YoutubeDL(options) as ydl:
 					ydl.download([url])
 				pass
@@ -35,15 +36,19 @@ class mixerbox:
 			
 
 	def getHTML(self,vectorId):
-		cto = urllib.request.urlopen('http://www.mixerbox.com/service?&callback=jQuery172009174624213601712_1453105380546&appVer=205&funcs=getVector&skip=0&limit=0&locale=zh-tw&mobile=1&type=playlist&vectorId='+vectorId+'&random=8398&_=1453105381259')
+		cto = urllib.request.urlopen('https://www.mixerbox.com/service?func=getVector&locale=zh-tw&type=playlist&uuid=dbb57c69-f753-4470-8024-2a1ea01135c8-web&vectorId=' + vectorId)
 		return cto.read().decode('utf-8')
 
+# print(listtt.aliist)
 mixerbox = mixerbox()
-InputUrl = input("請輸入收藏清單網址：")
-vectorId = mixerbox.getvectorId(InputUrl)
+# InputUrl = input("請輸入收藏清單網址：")
+# InputUrl = 'http://www.mixerbox.com/list/7742969'
+# vectorId = mixerbox.getvectorId(InputUrl)
 # vectorId = "7742969"
-HTML = mixerbox.getHTML(vectorId)
-result = re.search(r'\((.+?)\)\;',HTML).group(1)
-res = json.loads(result)
+# HTML = mixerbox.getHTML(vectorId)
+# result = re.search(r'\((.+?)\)\;',HTML).group(1)
+res = listtt.aliist
+# res = json.loads(result)
+# print(res)
 item = res["getVector"]["items"]
 mixerbox.dowload(item)
